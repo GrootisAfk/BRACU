71c9cc291a7caa2e0a561f68a47a772d5098bba0
Evan <93700322+GrootisAfk@users.noreply.github.com>
2025-03-02 14:56:45 +0600

    Add files via upload

---
+++ CSE110/Assignment 1/CIRCLE.py
@@ -0,0 +1,6 @@
+r = input("Enter the radius:")
+r = float(r)
+area = 3.1416 * r ** 2
+circumference = 2 * 3.1416 * r
+print("Area is", area)
+print("Circumference is", circumference)
---
+++ CSE110/Assignment 1/task 2.py
@@ -0,0 +1,12 @@
+n = input("Enter the first number:")
+n = float(n)
+m = input("Enter the second number:")
+m = float(m)
+if n > m :
+    print("First number is greater")
+elif n < m :
+    print("Second number is greater")
+else :
+    print("The numbers are equal")
+
+
